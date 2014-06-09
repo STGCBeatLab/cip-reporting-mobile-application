b7472e38e30c9103bc8667a10d74def70d6d9011
STGCBeatLab <jmorrow@stgcommission.com>
2014-06-09 15:58:57 -0700

    6-9-2014

---
+++ Customize/custom.js
@@ -0,0 +1,7 @@
+// JavaScript Document 
+$(function(){
+      $("#CustomHeader").load("./Customize/CustomHeader.html");
+	  $("#CustomFooter").load("./Customize/CustomFooter.html"); 
+	  $("#EUP") .load("./Customize/EUPModal.html");
+});
+    
