f00eb2ee04a84b8651337bb3f1eb80a432cbf1b8
STGCBeatLab <jmorrow@stgcommission.com>
2014-06-11 09:48:55 -0700

    6-11-2014

--- Customize/custom.js
+++ Customize/custom.js
@@ -5,3 +5,14 @@ $(function(){
 	  $("#EUP") .load("./Customize/EUPModal.html");
 });
     
+$('input, textarea')
+.on('focus', function (e) {
+    $('header, footer').css('position', 'absolute');
+})
+.on('blur', function (e) {
+    $('header, footer').css('position', 'fixed');
+    //force page redraw to fix incorrectly positioned fixed elements
+    setTimeout( function() {
+        window.scrollTo( $.mobile.window.scrollLeft(), $.mobile.window.scrollTop() );
+    }, 20 );
+});
